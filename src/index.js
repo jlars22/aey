11c9c99c6747535bc730ec60fdee2ea87db5d63e
Jonas Larsen <joonaslarsen@gmail.com>
2024-10-31 15:07:12 +0100

    Revert "add routes"
    This reverts commit 83fef6b751ecb947a7cd43776452f4f4e19f8658.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/simulation" element={<h1>About</h1>} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 );
 
