894fdd0783f490cf4eda2da80e06a5dcdb5f2366
Jonas Larsen <joonaslarsen@gmail.com>
2024-11-04 09:21:01 +0100

    progress

--- src/Constants.js
+++ src/Constants.js
@@ -1,3 +1,4 @@
 // https://sparenergi.dk/privat/energipriser-paa-sparenergi
 export const HEAT_PRICE_KWH = 0.84;
+export const HEAT_PRICE_KWH_2023 = 0.84;
 export const ELECTRICIY_TAX_RATE = 0.761;
--- src/Pages/Simulation.js
+++ src/Pages/Simulation.js
@@ -2,14 +2,30 @@ import { DateCalendar, LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import React, { useState, useEffect } from 'react';
 import dayjs from 'dayjs';
-import { Button, Typography } from '@mui/material';
+import {
+  Button,
+  Typography,
+  Table,
+  TableBody,
+  TableRow,
+  TableCell,
+  Stack,
+  TextField,
+  InputAdornment,
+} from '@mui/material';
 import BackButton from '../Components/BackButton';
 import { IoMdPlay } from 'react-icons/io';
 import { getPricesByDate } from '../Api/Elprisenligenu';
+import { HEAT_PRICE_KWH_2023 } from '../Constants';
+import { FaStop } from 'react-icons/fa';
+import { MdElectricBolt } from 'react-icons/md';
+import { LiaIndustrySolid } from 'react-icons/lia';
 
 export default function Simulation() {
   const [currentDate, setCurrentDate] = useState(dayjs('2023-01-01'));
   const [isRunning, setIsRunning] = useState(false);
+  const [savings, setSavings] = useState(0);
+  const [energyConsumption, setEnergyConsumption] = useState(4.53);
 
   useEffect(() => {
     let timer;
@@ -31,7 +47,12 @@ export default function Simulation() {
 
   const startSimulation = () => {
     setCurrentDate(dayjs('2023-01-01'));
+    setSavings(0);
     setIsRunning(true);
+  };
+
+  const stopSimulation = () => {
+    setIsRunning(false);
   };
 
   useEffect(() => {
@@ -40,30 +61,124 @@ export default function Simulation() {
 
       const data = await getPricesByDate(date);
 
-      console.log(data);
+      const totalSavings = data.reduce((acc, price) => {
+        if (price.DKK_per_kWh < HEAT_PRICE_KWH_2023) {
+          return (
+            acc + (HEAT_PRICE_KWH_2023 - price.DKK_per_kWh) * energyConsumption
+          );
+        }
+        return acc;
+      }, 0);
+
+      setSavings(savings + totalSavings);
     }
     fetchElectricityPrices();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currentDate]);
 
   return (
     <div className="App">
       <header className="App-header">
         <BackButton />
+
         <Typography variant="h3" style={{ fontWeight: 'bold' }}>
           SIMULATION FOR 2023
         </Typography>
+        <Stack spacing={2}>
+          <TextField
+            label="Energiforbrug"
+            variant="outlined"
+            slotProps={{
+              input: {
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <MdElectricBolt />
+                  </InputAdornment>
+                ),
+
+                endAdornment: (
+                  <InputAdornment position="end">kWh/h</InputAdornment>
+                ),
+              },
+            }}
+            value={energyConsumption}
+            onChange={(event) => {
+              setEnergyConsumption(event.target.value);
+            }}
+            helperText={
+              <>
+                Angiv dit forventede energiforbrug pr. time i kWh. <br />
+                Standardværdien er 4.53 kWh/h svarende til en gennemsnitlig
+                husstand.
+              </>
+            }
+          />
+          <TextField
+            disabled
+            label="Fjernevarmepris"
+            variant="outlined"
+            slotProps={{
+              input: {
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <LiaIndustrySolid />
+                  </InputAdornment>
+                ),
+
+                endAdornment: (
+                  <InputAdornment position="end">kr/kWh</InputAdornment>
+                ),
+              },
+            }}
+            value={HEAT_PRICE_KWH_2023}
+            helperText={<>Gennemsnitlig fjernvarmepris for 2023.</>}
+          />
+        </Stack>
+
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DateCalendar value={currentDate} readOnly views={['day']} />
+          <DateCalendar
+            value={currentDate}
+            readOnly
+            views={['day']}
+            sx={{ marginTop: '20px' }}
+          />
         </LocalizationProvider>
 
-        <Button
-          variant="contained"
-          onClick={startSimulation}
-          disabled={isRunning}
-          startIcon={<IoMdPlay />}
-        >
-          Start Simulation
-        </Button>
+        <Stack spacing={2}>
+          <Button
+            variant="contained"
+            onClick={startSimulation}
+            disabled={isRunning}
+            startIcon={<IoMdPlay />}
+          >
+            Start Simulation
+          </Button>
+
+          <Button
+            variant="contained"
+            onClick={stopSimulation}
+            disabled={!isRunning}
+            startIcon={<FaStop />}
+          >
+            Stop Simulation
+          </Button>
+        </Stack>
+
+        <Stack>
+          <Typography variant="h5" component="h5" gutterBottom>
+            Savings Summary
+          </Typography>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell component="th" scope="row">
+                  Total Savings (DKK)
+                </TableCell>
+                <TableCell>{savings.toFixed(2)} kr</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </Stack>
       </header>
     </div>
   );
