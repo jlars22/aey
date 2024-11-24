f52aebd408f772b215574cfb300a0fb730a7bb2a
Jonas Larsen <joonaslarsen@gmail.com>
2024-11-24 12:15:37 +0100

    fix homepage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,29 +1,54 @@
-import { Button, Typography } from '@mui/material'
+import { Button, Typography, Stack, Box, Card, CardContent } from '@mui/material'
 import { Link } from 'react-router-dom'
 import '../App.css'
+import React from 'react'
 
 function Home() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <Typography variant='h1' gutterBottom style={{ fontWeight: 'bold' }}>
-          AEY
-        </Typography>
-        <Link to='/today'>
-          <Button variant='contained' style={{ marginBottom: '30px', fontWeight: 'bold' }} size='large'>
-            Electricity vs. Districtheating
-          </Button>
-        </Link>
-        <Link to='/simulation'>
-          <Button variant='contained' style={{ marginBottom: '30px', fontWeight: 'bold' }} size='large'>
-            Simulation
-          </Button>
-        </Link>
-        <Link to='/solarcell'>
-          <Button variant='contained' style={{ fontWeight: 'bold' }} size='large'>
-            Solarcell Simulation
-          </Button>
-        </Link>
+        <Box
+          sx={{
+            textAlign: 'center',
+            padding: '20px',
+
+            borderRadius: '10px'
+          }}
+        >
+          <Typography variant='h1' style={{ fontWeight: 'bold' }}>
+            AEY
+          </Typography>
+
+          <Stack spacing={3} alignItems='center'>
+            <Link to='/today' style={{ textDecoration: 'none', width: '100%' }}>
+              <Button
+                variant='contained'
+                style={{ fontWeight: 'bold', width: '100%', borderRadius: '20px' }}
+                size='large'
+              >
+                Electricity vs. Districtheating
+              </Button>
+            </Link>
+            <Link to='/simulation' style={{ textDecoration: 'none', width: '100%' }}>
+              <Button
+                variant='contained'
+                style={{ fontWeight: 'bold', width: '100%', borderRadius: '20px' }}
+                size='large'
+              >
+                Simulation
+              </Button>
+            </Link>
+            <Link to='/solarcell' style={{ textDecoration: 'none', width: '100%' }}>
+              <Button
+                variant='contained'
+                style={{ fontWeight: 'bold', width: '100%', borderRadius: '20px' }}
+                size='large'
+              >
+                Solarcell Simulation
+              </Button>
+            </Link>
+          </Stack>
+        </Box>
       </header>
     </div>
   )
